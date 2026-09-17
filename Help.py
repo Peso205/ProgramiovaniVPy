a7df02c4d669a075d1e4f64db279d3b880bebc5e
Peso205 <205majo@gmail.com>
2026-09-17 16:41:18 +0200

    test

---
+++ Help.py
@@ -0,0 +1,3 @@
+import numpy as np
+
+print("Welcome to the Help module! This module provides assistance and guidance for using the application. Here are some common commands and their descriptions:")
